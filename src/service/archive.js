3f918bc4f990c63f90661d4997c4886bd8f02b9c
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-05-31 16:55:05 +0300

    added boilerplate for archive

--- src/service/archive.js
+++ src/service/archive.js
@@ -1,5 +1,20 @@
-const archive = async () => {
+const fetch = require('isomorphic-fetch')
 
+const archive = async (config, data) => {
+  return fetch(`${config.url}/abc/`, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `bearer ${config.token}`
+    }
+  }).then((response) => {
+    if (response.status !== 202) {
+      // TODO improve throwed errors
+      throw new Error('Failed to archive service')
+    }
+    return response
+  })
 }
 
 export default archive
