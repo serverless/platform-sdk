7efd93521392f4792947f4643c6b860648d12cc3
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-06-15 16:18:45 +0300

    run server inside login method

--- src/login/login.js
+++ src/login/login.js
@@ -4,14 +4,13 @@ const cors = require('cors')
 const querystring = require('querystring')
 const openBrowser = require('./openBrowser')
 
-const app = express()
-app.use(bodyParser.json())
-app.use(cors())
-const server = app.listen(8000)
-
 const PLATFORM_FRONTEND_BASE_URL = 'http://localhost:3000/'
 
 const login = async () => {
+  const app = express()
+  app.use(bodyParser.json())
+  app.use(cors())
+  const server = app.listen(8000)
   const queries = querystring.stringify({ cli: 'v2' })
 
   const opnRes = await openBrowser(`${PLATFORM_FRONTEND_BASE_URL}?${queries}`)
