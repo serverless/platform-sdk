6d2657568f511104569c4a6a866ca66ed3d59187
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-06-29 13:42:43 +0300

    add local stage

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
-const { SERVERLESS_PLATFORM_STAGE } = process.env || 'prod'
+const platformStage = process.env.SERVERLESS_PLATFORM_STAGE || 'prod'
 
 const config = {
+  local: {
+    frontendUrl: 'http://localhost:3000/',
+    backendUrl: 'https://5xe6lexe8e.execute-api.us-east-1.amazonaws.com/dev/'
+  },
   preview: {
     frontendUrl: 'https://deploy-preview-124--eg-dashboard-dev.netlify.com/',
     backendUrl: 'https://5xe6lexe8e.execute-api.us-east-1.amazonaws.com/dev/'
@@ -11,8 +15,8 @@ const config = {
   },
   prod: {
     frontendUrl: 'https://dashboard.serverless.com/',
-    backendUrl: 'https://5xe6lexe8e.execute-api.us-east-1.amazonaws.com/dev/'
+    backendUrl: 'https://jnvhp1any0.execute-api.us-east-1.amazonaws.com/prod/'
   }
 }
 
-module.exports = config[SERVERLESS_PLATFORM_STAGE]
+module.exports = config[platformStage]
