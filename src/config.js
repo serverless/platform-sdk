fc4bc6aba4414420fe32616976d2e5acfb52c37b
Ganesh <ganesh@serverless.com>
2018-06-29 16:25:44 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3,15 +3,15 @@ const platformStage = process.env.SERVERLESS_PLATFORM_STAGE || 'prod'
 const config = {
   local: {
     frontendUrl: 'http://localhost:3000/',
-    backendUrl: 'https://garkuu3a57.execute-api.us-east-1.amazonaws.com/dev'
+    backendUrl: 'https://garkuu3a57.execute-api.us-east-1.amazonaws.com/dev/'
   },
   preview: {
     frontendUrl: 'https://deploy-preview-124--eg-dashboard-dev.netlify.com/',
-    backendUrl: 'https://garkuu3a57.execute-api.us-east-1.amazonaws.com/dev'
+    backendUrl: 'https://garkuu3a57.execute-api.us-east-1.amazonaws.com/dev/'
   },
   dev: {
     frontendUrl: 'https://dashboard.serverless-dev.com/',
-    backendUrl: 'https://garkuu3a57.execute-api.us-east-1.amazonaws.com/dev'
+    backendUrl: 'https://garkuu3a57.execute-api.us-east-1.amazonaws.com/dev/'
   },
   prod: {
     frontendUrl: 'https://dashboard.serverless.com/',
