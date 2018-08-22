c0b178004a8efd47afc412337cfef130ae78c6c3
Eslam A. Hefnawy <eahefnawy@gmail.com>
2018-08-22 17:37:04 +0300

    added support for CI and github app

--- src/deployments/createDeployment.js
+++ src/deployments/createDeployment.js
@@ -3,12 +3,33 @@ const platformConfig = require('../config')
 const currentVersion = require('../../package.json').version
 
 const createDeployment = async (data) => {
+  const source = {}
+  const build = {}
+
+  if (process.env.TRAVIS) {
+    const [owner, repo] = process.env.TRAVIS_REPO_SLUG.split('/')
+    source.owner = owner
+    source.repo = repo
+    source.type = 'github'
+    source.sha = process.env.TRAVIS_COMMIT
+    build.type = 'travis'
+    build.id = process.env.TRAVIS_BUILD_ID
+  } else if (process.env.CI) {
+    source.owner = process.env.CI_SOURCE_OWNER
+    source.repo = process.env.CI_SOURCE_REPO
+    source.type = process.env.CI_SOURCE_TYPE
+    source.sha = process.env.CI_COMMIT
+    build.type = process.env.CI_TYPE
+    build.id = process.env.CI_BUILD_ID
+  }
+
   const response = await fetch(
     `${platformConfig.backendUrl}tenants/${data.tenant}/applications/${data.app}/services/${
       data.serviceName
     }/deployments`,
     {
       method: 'POST',
+      body: JSON.stringify({ source, build }),
       headers: {
         'Content-Type': 'application/json',
         'x-platform-version': currentVersion,
--- src/deployments/createDeployment.test.js
+++ src/deployments/createDeployment.test.js
@@ -29,6 +29,7 @@ describe('createDeployment', () => {
       }/deployments`,
       {
         method: 'POST',
+        body: JSON.stringify({ source: {}, build: {} }),
         headers: {
           'Content-Type': 'application/json',
           'x-platform-version': currentVersion,
@@ -36,5 +37,107 @@ describe('createDeployment', () => {
         }
       }
     )
+  })
+
+  test('it should make a valid request in Travis', async () => {
+    // CI setup
+    process.env.TRAVIS = true
+    process.env.TRAVIS_REPO_SLUG = 'serverless/service'
+    process.env.TRAVIS_COMMIT = 'abc'
+    process.env.TRAVIS_BUILD_ID = 'xyz'
+
+    const source = {
+      owner: 'serverless',
+      repo: 'service',
+      type: 'github',
+      sha: 'abc'
+    }
+    const build = {
+      type: 'travis',
+      id: 'xyz'
+    }
+
+    const data = {
+      tenant: 'sometenant',
+      app: 'someapp',
+      serviceName: 'someservicename',
+      accessKey: 'someaccesskey'
+    }
+
+    await createDeployment(data)
+
+    expect(fetch).toBeCalledWith(
+      `${platformConfig.backendUrl}tenants/${data.tenant}/applications/${data.app}/services/${
+        data.serviceName
+      }/deployments`,
+      {
+        method: 'POST',
+        body: JSON.stringify({ source, build }),
+        headers: {
+          'Content-Type': 'application/json',
+          'x-platform-version': currentVersion,
+          Authorization: `bearer ${data.accessKey}`
+        }
+      }
+    )
+
+    delete process.env.TRAVIS
+    delete process.env.TRAVIS_REPO_SLUG
+    delete process.env.TRAVIS_COMMIT
+    delete process.env.TRAVIS_BUILD_ID
+  })
+
+  test('it should make a valid request in any CI', async () => {
+    // CI setup
+    process.env.CI = 'true'
+    process.env.CI_SOURCE_OWNER = 'serverless'
+    process.env.CI_SOURCE_REPO = 'service'
+    process.env.CI_SOURCE_TYPE = 'bitbucket'
+    process.env.CI_COMMIT = 'abc'
+    process.env.CI_TYPE = 'someCISystem'
+    process.env.CI_BUILD_ID = 'xyz'
+
+    const source = {
+      owner: 'serverless',
+      repo: 'service',
+      type: 'bitbucket',
+      sha: 'abc'
+    }
+    const build = {
+      type: 'someCISystem',
+      id: 'xyz'
+    }
+
+    const data = {
+      tenant: 'sometenant',
+      app: 'someapp',
+      serviceName: 'someservicename',
+      accessKey: 'someaccesskey'
+    }
+
+    await createDeployment(data)
+
+    expect(fetch).toBeCalledWith(
+      `${platformConfig.backendUrl}tenants/${data.tenant}/applications/${data.app}/services/${
+        data.serviceName
+      }/deployments`,
+      {
+        method: 'POST',
+        body: JSON.stringify({ source, build }),
+        headers: {
+          'Content-Type': 'application/json',
+          'x-platform-version': currentVersion,
+          Authorization: `bearer ${data.accessKey}`
+        }
+      }
+    )
+
+    delete process.env.CI
+    delete process.env.CI_SOURCE_OWNER
+    delete process.env.CI_SOURCE_REPO
+    delete process.env.CI_SOURCE_TYPE
+    delete process.env.CI_COMMIT
+    delete process.env.CI_TYPE
+    delete process.env.CI_BUILD_ID
   })
 })
