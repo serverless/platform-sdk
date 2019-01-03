ede4d161e1795f33bc23a174ca58d8a0aa164e55
Eslam A. Hefnawy <eahefnawy@gmail.com>
2019-01-03 15:22:56 +0300

    added getAccessKey util

--- src/rcfile.js
+++ src/rcfile.js
@@ -19,3 +19,17 @@ export const getUser = () => {
   }
   return pick(['idToken', 'username'], user)
 }
+
+export const getAccessKey = (tenant) => {
+  const config = getConfig()
+  const user = get(['users', config.userId, 'dashboard'], config)
+  if (!user || !user.username || !user.idToken) {
+    // user is logged out
+    return null
+  }
+
+  // to avoid "cannot read from undefined" error...
+  user.accessKeys = user.accessKeys || {}
+
+  return process.env.SERVERLESS_ACCESS_KEY || user.accessKeys[tenant]
+}
